55e0f7597a0208316647160ae2f0c898f497b563
sgarg2023 <shubhrag2021@gmail.com>
2023-07-01 11:22:27 +0530

    added more PHP files

--- ifelse.php
+++ ifelse.php
@@ -20,6 +20,18 @@
             var_dump($article);
         }
 
+        echo"\nLets try printing hours:\n";
+        $hour=11;
+        if($hour <12){
+            echo "\nGood morning\n";
+        }elseif($hour <18){
+            echo "\nGood afternoon\n";
+        }elseif($hour <22){
+            echo "\nGood evening\n";
+        }else{
+            echo "\nGood night\n";
+        }
+
     ?>
 </body>
 </html>
